ae12205318601ebd746aaab275cab89066c95d3a
Alonso Cárdenas <alcardenascastroamador@gmail.com>
2026-03-31 13:23:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
+import requests
+from bs4 import BeautifulSoup
+
 
 app = FastAPI()
 
-
-
 @app.get("/time")
 def get_current_time():
-    return {"current_time": }
+    url= "https://www.horaoficial.cl/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.txt, 'html.parser')
+    for equisde in soup.find_all("div"):
+        print(equisde.getText)
+    return {"current_time": "ola"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
