139b5fef7cc63dca3c86352a39a6a3e91bceba8c
Alonso Cárdenas <alcardenascastroamador@gmail.com>
2026-04-01 20:47:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ def get_current_time():
     response.offset
     chile_tz = pytz.timezone('America/Santiago')
     current_time = datetime.fromtimestamp(response.tx_time, chile_tz)
-    return {"current_time": current_time }
+    return {"Año-Mes-Día": f"{current_time.year}-{current_time.month}-{current_time.day}",
+            "Hora:Minutos:Segundo": f"{current_time.hour}:{current_time.minute}:{current_time.second}"
+            }
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
